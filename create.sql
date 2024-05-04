c6ac7e3ffac4a3ffed7cee53774cc4302288131c
lakshaysharma399 <lakshaysharma399@gmail.com>
2024-05-04 19:10:48 +0530

    First query

--- create.sql
+++ create.sql
@@ -2,9 +2,8 @@ create database pizzahut;
 
 create table order_details (
 order_details_id int not null,
-order_id int not nu,
+order_id int not null,
 pizza_id text not null,
 quantity int not null,
 primary key(order_details_id)
 ); 
--- heelo
